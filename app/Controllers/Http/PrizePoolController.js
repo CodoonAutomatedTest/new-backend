78a2fe19f6186e26497c18bffaf43a88546c2d8a
Mr.x <xiaoqiang@codoon.com>
2019-10-06 15:38:21 +0800

    fix bug

--- app/Controllers/Http/PrizePoolController.js
+++ app/Controllers/Http/PrizePoolController.js
@@ -5,7 +5,7 @@ const PrizePool = use('App/Models/PrizePool')
 class PrizePoolController {
   async create({auth, request}) {
     const user = await auth.getUser()
-    const {code, name, avatar, prob, num} = request.all()
+    const {code, name, avatar, head, prob, num} = request.all()
     return await PrizePool.create({
       code,
       name,
