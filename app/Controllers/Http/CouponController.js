5d611102c0224e4a76330fcfd71cae575fce9e9a
Mr.x <xiaoqiang@codoon.com>
2019-10-16 19:02:54 +0800

    fix bugs

--- app/Controllers/Http/CouponController.js
+++ app/Controllers/Http/CouponController.js
@@ -28,7 +28,7 @@ class CouponController {
       })
     }
     context = JSON.parse(context)
-    const user = await User.findBy({'id': coupon.user_id})
+    const user = await User.findBy({'id': context.user_id})
     return response.status(200).json({
       'id': user.id,
       'user_id': user.user_id,
