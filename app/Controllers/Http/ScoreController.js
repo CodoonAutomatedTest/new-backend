2ec50df5019c5129d1be00e8285c700771947380
Mr.x <xiaoqiang@codoon.com>
2019-10-16 22:53:17 +0800

    交易积分

--- app/Controllers/Http/ScoreController.js
+++ app/Controllers/Http/ScoreController.js
@@ -6,6 +6,12 @@ class ScoreController {
     const flow = await user.scores().orderBy('current', 'desc').fetch()
     return flow
   }
+
+  async deal ({request, response}) {
+    const {id, score} = request.all()
+    let currentDate = DateGenerator.getNowFormatDate()
+    return await Score.create({user_id: id, 'current': currentDate, value:parseInt(score) , info: '消费积分'})
+  }
 }
 
 module.exports = ScoreController
