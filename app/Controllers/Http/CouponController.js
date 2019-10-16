3e2eecee70464763327946f9bf8704ebedf6cebd
Mr.x <xiaoqiang@codoon.com>
2019-10-16 18:33:02 +0800

    fix bugs

--- app/Controllers/Http/CouponController.js
+++ app/Controllers/Http/CouponController.js
@@ -27,7 +27,7 @@ class CouponController {
         message: 'invaild id, check it please!'
       })
     }
-    return context.user_id
+    return context
     const user = await User.findBy({'id': context.user_id})
     return response.status(200).json({
       'id': user.id,
